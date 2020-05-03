8f30a5af82cfb0d3d8006a4480762ca02fa2751b
Ayush garg <64222620+ayushgarg27@users.noreply.github.com>
2020-05-03 19:10:43 +0530

    Add files via upload

---
+++ aoc.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main(){
+float r;
+float area;
+printf("Enter the radius : ");
+scanf("%f", &r);
+area = 3.14 * r *r ;
+printf("The area of the circle is : ");
+printf("%f" ,area);
+return 0;
+}
+
